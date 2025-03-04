ee810bd6636c1e53572d9b54d961c54e41a0c9f0
703lovelost <dolphinhunter@inbox.ru>
2025-03-04 15:40:28 +0700

    Minor changes.

--- parser/parser.py
+++ parser/parser.py
@@ -1,3 +1,6 @@
+import time
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,9 +9,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from selenium.webdriver.chrome.options import Options
-import csv
+
 
 
 url = 'https://forum.vgd.ru/1693/115157/0.htm'
